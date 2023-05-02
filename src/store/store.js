a3bfa0630b885bf6c197bcaa6295b432f0bee86f
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-05-02 16:15:02 +0300

    Enabling additional reducer using middleware and redux-logger

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,22 @@
 import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import {reducer as favoritesReducer} from "./favorites/favorites.slice.js";
+import {userSlice} from "./user/user.slice.js";
+import {api} from "./api/api.js";
+import {createLogger} from "redux-logger";
+
+
+const logger = createLogger({
+    collapsed:true
+})
 
 const rootReducer = combineReducers({
     favorites: favoritesReducer,
+    user:userSlice.reducer,
+    [api.reducerPath]: api.reducer
 });
 
 export const store = configureStore({
     reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(api.middleware).concat(logger)
 });
